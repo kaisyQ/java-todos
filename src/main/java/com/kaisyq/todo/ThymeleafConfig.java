0216a052ad94af96f1db6831a89f5dc9c55676c0
kaisyQ <rutyo8@mail.ru>
2024-07-13 17:02:58 +0300

    add configs for templates

---
+++ src/main/java/com/kaisyq/todo/ThymeleafConfig.java
@@ -0,0 +1,23 @@
+package com.kaisyq.todo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+    
+    @Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("com/kaisyq/todo/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+}
---
+++ src/main/java/com/kaisyq/todo/controllers/TodosController.java
@@ -0,0 +1,23 @@
+package com.kaisyq.todo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.TemplateEngine;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequestMapping("/todos")
+@Controller
+public final class TodosController {
+    
+    @GetMapping("")
+    public String getList(Model model) {
+        model.addAttribute("message", "Thank you for visiting.");
+
+        return "index";
+    }
+}
